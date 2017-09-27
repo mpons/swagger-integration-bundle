9985c6a533f0e6ce16eca8f4235d13d82fe4b3de
Matthieu Pons <m.pons@rebuy.com>
2017-09-27 14:23:13 +0200

    First Commit

---
+++ src/Mpons/SwaggerIntegrationBundle/EventListener/AnnotationListenner.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mpons\SwaggerIntegrationBunble\EventListener;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Mpons\SwaggerIntegrationBunble\Annotation\SwaggerEndpoint;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class AnnotationListener
+{
+    protected $reader;
+    protected $annotationClass = SwaggerEndpoint::class;
+
+    public function __construct($reader)
+    {
+        /**
+         * @var AnnotationReader $reader
+         */
+        $this->reader = $reader;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+
+    }
+}
